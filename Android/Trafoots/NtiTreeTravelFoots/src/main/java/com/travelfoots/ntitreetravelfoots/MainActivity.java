5a2a879bcdb1be179da26464a66d61361149778f
leehojun <ghwnsqoqo@naver.com>
2018-06-04 21:51:24 +0900

    .

--- Android/Trafoots/NtiTreeTravelFoots/src/main/java/com/travelfoots/ntitreetravelfoots/MainActivity.java
+++ Android/Trafoots/NtiTreeTravelFoots/src/main/java/com/travelfoots/ntitreetravelfoots/MainActivity.java
@@ -181,15 +181,13 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-
+        
         //메뉴
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
 
         //지도
-
         Mapbox.getInstance(this, getString(R.string.access_token));
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
