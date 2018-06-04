523a7ca009022072c8aaeabb6bd58618111a413d
simchangbo <com@192.168.213.1>
2018-06-04 22:43:46 +0900

    원래 걸로 합치기

--- TeamTravel/src/com/tf/presentation/PhotoController.java
+++ TeamTravel/src/com/tf/presentation/PhotoController.java
@@ -1,1 +1,5 @@
+package com.tf.presentation;
 
+public class PhotoController {
+
+}
--- TeamTravel/src/com/tf/service/PinpointService.java
+++ TeamTravel/src/com/tf/service/PinpointService.java
@@ -7,6 +7,7 @@ import com.tf.domain.Pinpoint;
 public interface PinpointService {
 	public List<Pinpoint> list(Pinpoint pinpoint) throws Exception;
 	public Pinpoint view(Pinpoint pinpoint) throws Exception;
+	public boolean add(Pinpoint pinpoint) throws Exception;
 	public boolean edit(Pinpoint pinpoint) throws Exception;
 	public boolean delete(Pinpoint pinpoint) throws Exception;
 }
