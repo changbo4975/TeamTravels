7dda584cd7a1c834311dc1ea8c70eb46267a6f7a
KimWanGak <kyk2430@gmail.com>
2018-06-06 00:22:01 +0900

    Remove WebProj

--- TeamTravel/src/com/tf/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.tf.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface LoginService {
-	public Object login(String userEmail, String password);
-	public void logout(HttpServletRequest request);
-}
